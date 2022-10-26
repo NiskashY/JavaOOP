4ebde1afde92732721b709b2a0357e4b7e73e9ec
NiskashY <roman.pantus@mail.ru>
2022-10-26 07:52:06 +0300

    (#lab4) done subtask2

--- second_term/Lab4_StreamAPI/src/Task2.java
+++ second_term/Lab4_StreamAPI/src/Task2.java
@@ -1,4 +1,4 @@
-import jdk.jshell.spi.ExecutionControlProvider;
+import com.sun.source.tree.Tree;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,6 +7,54 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 import java.util.TreeMap;
+
+import static java.lang.Math.abs;
+
+class Storage extends TreeMap {
+    private TreeMap<Integer, Integer> count;
+
+    public Storage() {
+        count = new TreeMap<>();
+    }
+
+    public void Add(Integer number) {
+//        count.put(number, count.get(number) + 1);
+        int x = 0; // current value of number in map
+        if (count.containsKey(number)) {
+            x = count.get(number);
+        }
+
+        count.put(number, x + 1);
+    }
+
+    public void Remove(Integer number) {
+        if (!count.containsKey(number)) {
+            String msg_not_found = "No such element in map";
+            System.out.println(msg_not_found);
+        } else {
+            count.put(number, count.get(number) - 1);
+            if (count.get(number) == 0) {
+                count.remove(number);
+            }
+        }
+    }
+
+    public Integer FindNearest(Integer number) {
+        Integer nearest = null;
+        Integer first = count.floorKey(number);       // lower_bound
+        Integer second = count.higherKey(number);     // upper_bound
+
+        if (first != null && second != null) {
+            var first_abs = abs(number - first);
+            var second_abs = abs(number - second);
+            nearest = (first_abs <= second_abs ? first : second);
+        } else {
+            nearest = (first == null ? second : first);
+        }
+
+        return nearest;
+    }
+}
 
 public class Task2 {
     static public void Solve() {
@@ -41,7 +89,7 @@ public class Task2 {
     }
 
     static private void SubTask2() {
-        TreeMap<Integer, Integer> count = new TreeMap<>();
+        Storage storage = new Storage();
         String[] menu = {"1 - add Integer",
                 "2 - remove Integer",
                 "3 - find nearest Element",
@@ -49,45 +97,44 @@ public class Task2 {
 
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = 0;
+
         while (true) {
             int choice = Choice.InputOptional(menu);
             try {
-                switch (choice) {
-                    case 1 -> {
-                        // add
-                        final String input_msg = "Input integer: ";
-                        System.out.print(input_msg);
-                        int number = Integer.parseInt(reader.readLine());
-                        count.put(number, count.get(number) + 1);
-                    }
-                    case 2 -> {
-                        // remove
-                        final String input_msg = "Input integer: ";
-                        System.out.print(input_msg);
-                        int number = Integer.parseInt(reader.readLine());
-
-                        if (count.get(number) == 0) {
-                            String msg_not_found = "No such element in map";
-                            System.out.println(msg_not_found);
-                        } else {
-                            count.put(number, count.get(number) - 1);
-                            if (count.get(number) == 0) {
-
-                            }
-                        }
-                    }
-                    case 3 -> {
-                        // find
-                    }
-                    default -> {
-                        return;
-                    }
-                }
-            } catch (NumberFormatException e){
+                final String input_msg = "Input integer: ";
+                System.out.print(input_msg);
+                number = Integer.parseInt(reader.readLine());
+            } catch (NumberFormatException e) {
                 System.out.println("Wrong data type!");
-            }catch (IOException e) {
+                continue;
+            } catch (IOException e) {
                 System.out.println(e.getMessage());
             }
+
+            switch (choice) {
+                case 1 -> {
+                    // add
+                    storage.Add(number);
+                }
+                case 2 -> {
+                    // remove
+                    storage.Remove(number);
+                }
+                case 3 -> {
+                    // find
+                    Integer nearest = storage.FindNearest(number);
+                    if (nearest == null) {
+                        System.out.println("Storage is Empty");
+                    } else {
+                        System.out.println("Nearest element = " + nearest);
+                    }
+                }
+                default -> {
+                    return;
+                }
+            }
+
         }
     }
 }
